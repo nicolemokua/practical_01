d2b073e93995c9a655c2b21c217ad463197ac566
Your Name <you@example.com>
2024-09-03 13:09:02 +0300

    constructed a variable for name, age and a boolean that checks on whether or not i am a student

---
+++ variables_datatypes.js/01.js
@@ -0,0 +1,9 @@
+// Declare variables to store name, age, and a boolean value indicating if you're a student
+let name = "Nicole";
+let age = 20; 
+let isStudent = true;
+
+// Log each of these variables to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
