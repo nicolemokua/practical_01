acccdcc224f504552ce61e54ca9d4bd90b60a824
Your Name <you@example.com>
2024-09-03 13:30:56 +0300

    converting a string that represents a number to an actual number type while add 10 and creating varibles with a null value and undefined value

---
+++ variables_datatypes.js/02.js
@@ -0,0 +1,7 @@
+// Convert a string representing a number to an actual number type and add 10 to it
+let stringNumber = "4";
+let convertNumber = Number(stringNumber); // Convert string to number
+let result = convertNumber + 10; // Add 10
+console.log(convertNumber);
+console.log("Result:", result);
+
---
+++ variables_datatypes.js/03.js
@@ -0,0 +1,6 @@
+// Create a variable with a null value and a variable with an undefined value
+let nullValue = null;
+let undefinedValue;
+
+console.log( nullValue);
+console.log(undefinedValue);
